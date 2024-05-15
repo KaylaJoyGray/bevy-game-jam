9dc10815be08031f7b110b11543e09b30ed58073
Kayla Gray <kjoygray75@gmail.com>
2024-05-14 21:33:41 -0500

    Added TODO OTD

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod gfx;
 mod ron_helpers;
 mod sound;
 
+// TODO: OTD: Organize SFX and sound into plugins. Add Player entity/start controls.
+
 #[wasm_bindgen(start)]
 pub fn start() {
     App::new().add_plugins(DefaultPlugins).run()
