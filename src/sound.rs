6209bdfb943bd6129cdaaf3f1e9d7a5e8e8538d5
Kayla Gray <kjoygray75@gmail.com>
2024-05-14 20:50:45 -0500

    Added load_sounds system

--- src/sound.rs
+++ src/sound.rs
@@ -1,12 +1,12 @@
-use bevy::prelude::{Asset, AudioSource, Decodable, Handle, Resource};
+use crate::ron_helpers::{parse, trim_extension};
+use bevy::asset::AssetServer;
+use bevy::log::info;
+use bevy::prelude::{AudioSource, Commands, Handle, Res, Resource};
 use bevy::utils::HashMap;
 
 #[derive(Debug, Resource)]
-pub struct SoundResource<Source = AudioSource>
-where
-    Source: Asset + Decodable,
-{
-    pub map: HashMap<String, Handle<Source>>,
+pub struct SoundResource {
+    pub map: HashMap<String, Handle<AudioSource>>,
 }
 
 impl SoundResource {
@@ -15,4 +15,27 @@ impl SoundResource {
             map: HashMap::new(),
         }
     }
+}
+
+///
+/// load_sounds: Bevy system
+///
+/// This system scans the graphics folder for sprite sheets and loads the resources
+/// to the asset server
+///
+pub fn load_sounds(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let config = parse::<Vec<(String)>>("./assets/sounds/config.ron")
+        .expect("Fatal: could not parse sounds/config.ron");
+
+    let mut sound_resource = SoundResource::new();
+
+    config.iter().for_each(|data| {
+        let handle = asset_server.load(data);
+
+        sound_resource.map.insert(trim_extension(&data), handle);
+
+        info!("Loaded sound file: {}", data);
+    });
+
+    commands.insert_resource(sound_resource);
 }
