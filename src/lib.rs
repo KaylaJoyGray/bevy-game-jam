f07184cabd88e84394bd2921aae5c16478c2d55f
Kayla Gray <kjoygray75@gmail.com>
2024-05-14 22:30:56 -0500

    Got rid of TODO OTD

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ mod gfx;
 mod ron_helpers;
 mod sound;
 
-// TODO: OTD: Organize SFX and sound into plugins. Add Player entity/start controls.
-
 #[wasm_bindgen(start)]
 pub fn start() {
     App::new().add_plugins(DefaultPlugins).run()
